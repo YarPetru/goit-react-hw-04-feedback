8836a1ff310d9570c3ed6def391ec6442a2d6428
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-06-10 22:44:31 +0300

    proptypes added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import s from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
@@ -7,9 +8,17 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
       <li>Neutral: {neutral}</li>
       <li>Bad: {bad}</li>
       <li>Total: {total} </li>
-      <li>Positive Feedback:{positivePercentage}</li>
+      <li>Positive Feedback: {positivePercentage}</li>
     </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
 };
 
 export default Statistics;
